8c7fcc247b72f17fd0f8f5dbebaacafcee61b127
rajdbs32 <rajdbs32@gmail.com>
2023-09-22 14:49:14 +0530

    added library

--- fetchingdata.py
+++ fetchingdata.py
@@ -90,3 +90,7 @@ print(data.head())
 
 ###############################stage 3##############################
 ###ML model####
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
